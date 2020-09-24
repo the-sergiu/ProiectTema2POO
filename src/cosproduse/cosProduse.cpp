c04b32bc38b18bfbbfd7d9db3167b06b1fc8e283
Constantin Barac <ilconstantinbarac@gmail.com>
2020-09-24 21:35:15 +0300

    Added missing setters and getters for CosProduse

--- src/cosproduse/cosProduse.cpp
+++ src/cosproduse/cosProduse.cpp
@@ -1,5 +1,24 @@
 #include "cosProduse.h"
 
+CosProduse::CosProduse()
+{
+  this->metodaPlata = "-";
+}
+
+CosProduse::CosProduse(const string& metodaPlata)
+{
+  this->metodaPlata = metodaPlata;
+}
+
+string& CosProduse::getMetodaPlata()
+{
+  return this->metodaPlata;
+}
+
+void CosProduse::setMetodaPlata(const string& metodaPlata)
+{
+  this->metodaPlata = metodaPlata;
+}
 
 void CosProduse::addProdus(int id, int cantitate)
 {
@@ -16,13 +35,19 @@ void CosProduse::deleteProdus(int produsID)
   this->produse.erase(produsID);
 }
 
+unordered_map<int, int>& CosProduse::getCos() {
+  return this->produse;
+}
+
 void CosProduse::afisare()
 {
-  if (this->produse.empty())
+  if (this->produse.empty()) {
     cout << "Cosul de produse este gol";
+  }
   else
   {
     cout<<"ProdusID:    ///   Cantitate:"<<endl;
+
     for(auto const& x : this->produse)
       cout<<x.first<<" "<<x.second<<endl;
   }
